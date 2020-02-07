4e9d4233d5cb8fe99130d383e2cc5e108f403507
coltonhurst <colton@coltonhurst.com>
2020-02-07 12:23:42 -0500

    Complete window example

--- SDL_Window/SDL_Window/main.cpp
+++ SDL_Window/SDL_Window/main.cpp
@@ -1,7 +1,64 @@
 #include <iostream>
+#include <SDL2/SDL.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+const int WINDOW_HEIGHT = 640;
+const int WINDOW_WIDTH = 480;
+
+// Starting point.
+int main(int argc, const char * argv[])
+{
+    // Create the window & screen surface.
+    // We render to the window & the surface is "contained" by the window.
+    SDL_Window* window = nullptr;
+    SDL_Surface* screenSurface = nullptr;
+    
+    // Initiate SDL. If an error occurs, stop & print the error.
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cout << "SDL could not be initialized. SDL_Error: " << SDL_GetError() << std::endl;
+    }
+    else
+    {
+        // Create the window.
+        window = SDL_CreateWindow("Window Title", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_HEIGHT, WINDOW_WIDTH, SDL_WINDOW_SHOWN);
+        
+        // If the window can't be created, print the error.
+        if (window == nullptr)
+        {
+            std::cout << "The window couldn't be created. SDL_Error: " << SDL_GetError() << std::endl;
+        }
+        else
+        {
+            // Get the window's surface.
+            screenSurface = SDL_GetWindowSurface(window);
+            
+            // Make the screen surface white.
+            SDL_FillRect(screenSurface, nullptr, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+            
+            SDL_Event event;
+            bool quit = false;
+            
+            // EVENT LOOP
+            while ((SDL_PollEvent(&event) != 0) && (!quit))
+            {
+                if (event.type == SDL_QUIT)
+                {
+                    quit = true;
+                }
+                else
+                {
+                    // Update the surface.
+                    SDL_UpdateWindowSurface(window);
+                }
+            }
+        }
+    }
+    
+    // Destroy the window.
+    SDL_DestroyWindow( window );
+
+    // Quit SDL.
+    SDL_Quit();
+    
     return 0;
 }
